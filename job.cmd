72c7616610bba2120f6643c43f76306a6ca83d16
cgonzalez90 <gonzalez.t@atinet.es>
2017-06-08 15:44:18 +0200

    Update job.cmd

--- job.cmd
+++ job.cmd
@@ -6,6 +6,5 @@
 #BSUB -J 1PPE
 #BSUB -W 18:00
 #BSUB -q bsc_ls
-
 module load intel openmpi python mkl
 mpirun lightdock RRRR_rec.pdb LLLL_lig.pdb 400 300 200 -f glowworm.conf -mpi -s scoring.conf -nm
